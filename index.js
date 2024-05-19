620e01b6f3fe2f281e44e6f3a7d7836dfefa9867
luqaa18 <lumartinza321@gmail.com>
2024-05-18 23:43:30 -0300

    implementado el carrito de compras mediante una funcion

--- index.js
+++ index.js
@@ -1,47 +1,117 @@
-let user = prompt("Bienvenido a Valentime Nails, ingrese su nombre para iniciar");
+//CONSTRUCTORES DE LOS SERVICIOS Y PRODUCTOS A OFRECER
 
-while (user == ""){
-    alert(`Para continuar, debes ingresar un nombre`);
-    user = prompt("Ingresa un nombre")
-}
-alert(`Bienvenido ${user} a Valentime Nails, donde tu cuidado es nuestra prioridad 1°.`);
-
-let services = true;
-let francesas = 4000;
-let semipermanente = 4200;
-let esculpidas = 5000;
-let polygel = 7500;
-
-let total = 0;
-let finalList= "";
-
-while (services){
-    let serviceOptions = parseInt(prompt(`${user}, a continuación elija el servicio que desea.\n1. Francesas: $4000\n2. Semipermanentes: $4200\n3. Esculpidas: $5000\n4. Polygel:$7500\n5. Terminar cotización`));
-    switch (serviceOptions){
-        case 1: 
-            total += francesas
-            finalList += "Francesas\n"
-        break;
-        case 2: 
-            total += semipermanente
-            finalList += "Semipermanentes\n"
-        break;
-        case 3: 
-            total += esculpidas
-            finalList += "Esculpidas\n"
-        break;
-        case 4: 
-            total += polygel
-            finalList += "Polygel\n"
-        break;
-        case 5: 
-            console.log(`Los servicios que elegiste ${user.toUpperCase()}, son:\n${finalList}Total de los servicios: $${total}`)
-            alert(`Los servicios que elegiste ${user}, son\n${finalList}Total de los servicios: $${total}`)
-            services = false;
-        break;
-        default:
-           alert("Ingrese una opción válida");
-        break;
+class Product {
+    constructor(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 }
-alert(`Nos alegra que nos hayas elegido ${user}, esperamos volver a verte pronto!`)
+
+class Service {
+    constructor(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+
+let productsList = [
+    new Product(1, "Esmalte gelificado", 2500),
+    new Product(2, "Esmalte semipermanente", 3500),
+    new Product(3, "Lima", 850),
+];
+
+let servicesList = [
+    new Service(1, "Semipermanente", 6000),
+    new Service(2, "Polygel", 5000),
+    new Service(3, "Francesas", 3500),
+    new Service(4, "Esculpidas", 7000),
+];
+
+let shoppingCart = [];
+let continueShopping = true;
+
+function displayCart() {
+    if (shoppingCart.length === 0) {
+        console.log("Tu carrito está vacío.");
+    } else {
+        let cartDetails = '';
+        let total = 0;
+        shoppingCart.forEach(item => {
+            cartDetails += `- ${item.name}: $${item.price}\n`;
+            total += item.price;
+        });
+        console.log(`Tu carrito:\n${cartDetails}\nTotal: $${total}`);
+    }
+}
+
+function productsMenu() {
+    let cycle = true;
+    while (cycle) {
+        let choice = parseInt(prompt(`A continuación se encuentran los productos que tenemos disponibles.\n\nElija una opción si quiere agregar al carrito:\n1. Esmalte gelificado: $2500\n2. Esmalte semipermanente: $3500\n3. Lima: $850\n4. Ver carrito\n5. Volver al menú anterior`));
+        switch (choice) {
+            case 1:
+            case 2:
+            case 3:
+                shoppingCart.push(productsList.find(product => product.id === choice));
+                break;
+            case 4:
+                displayCart();
+                break;
+            case 5:
+                cycle = false;
+                break;
+            default:
+                alert("Elija una opción válida");
+                break;
+        }
+    }
+}
+
+function servicesMenu() {
+    let cycle = true;
+    while (cycle) {
+        let choice = parseInt(prompt("A continuación se encuentran los servicios que ofrecemos.\n\nElija una opción si quiere agregar al carrito:\n1. Semipermanente: $6000\n2. Polygel: $5000\n3. Francesas: $3500\n4. Esculpidas: $7000\n5. Ver carrito\n6. Volver al menú anterior"));
+        switch (choice) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                shoppingCart.push(servicesList.find(service => service.id === choice));
+                break;
+            case 5:
+                displayCart();
+                break;
+            case 6:
+                cycle = false;
+                break;
+            default:
+                alert("Elija una opción válida");
+                break;
+        }
+    }
+}
+
+while (continueShopping) {
+    let firstMenu = parseInt(prompt("Bienvenido a Valentime Nails, donde el cuidado de sus manos es nuestra principal prioridad\n\nA continuación, elija una opción:\n1. Ver productos\n2. Ver servicios\n3. Ver carrito\n4. Salir"));
+    switch (firstMenu) {
+        case 1:
+            productsMenu();
+            break;
+        case 2:
+            servicesMenu();
+            break;
+        case 3:
+            displayCart();
+            break;
+        case 4:
+            continueShopping = false;
+            break;
+        default:
+            alert("Elija una opción válida");
+            break;
+    }
+}
+
+console.log("Gracias por visitar Valentime Nails!");
